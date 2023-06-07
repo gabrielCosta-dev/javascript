d97ca59b572edf658cd8246a4d7c903043651613
gabrielCosta-dev <lima20gabriel@gmail.com>
2023-06-07 15:28:25 -0300

    condições

---
+++ condicoes.js
@@ -0,0 +1,14 @@
+/* let vel = 70.5
+console.log(`A velocidade do seu carro é ${vel} km/hr.`)
+
+if (vel > 60.5) {
+    console.log(`Você foi multado por ultrapassar o limite de velocidade.`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`) */
+
+let pais = 'Brasil'
+if (pais == 'Brasil') {
+    console.log(`Brasileiro`)
+} else {
+    console.log('Gringo')
+}
