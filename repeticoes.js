d994a1b35d2a2f729dd1c4568d64076e2814ebcf
gabrielCosta-dev <lima20gabriel@gmail.com>
2023-06-19 15:34:00 -0300

    repetições

---
+++ repeticoes.js
@@ -0,0 +1,19 @@
+/* let c = 1
+
+while (c < 6) {
+    console.log('Tudo bem?')
+    c++
+}
+
+let fatias = 3
+
+while (fatias > 0) {
+    console.log('Ainda há fatias')
+    fatias--
+} */
+let c = 1
+
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 10)
