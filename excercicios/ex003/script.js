95ec6e0798a29fb154992b7e69514049c6b29113
gabrielCosta-dev <lima20gabriel@gmail.com>
2023-06-29 15:55:02 -0300

    Update script.js

--- excercicios/ex003/script.js
+++ excercicios/ex003/script.js
@@ -1,47 +1,47 @@
-function calcularPassos(){
+function calcularPassos() {
     let inicio = Number(document.querySelector('input#inicio').value);
     let fim = Number(document.querySelector('input#fim').value);
     let passo = Number(document.querySelector('input#passo').value);
-
-    if (inicio !== '' && fim !== '' && passo !== '' && passo > 0 && fim > inicio && fim > 0) {
-        inicio = Number(inicio);
-        fim = Number(fim);
-        passo = Number(passo);
-
-        if (!isNaN(inicio) && passo !== 0) {
-            let resultado = document.getElementById('resultado');
-            resultado.innerHTML = '';
-    
-            while (inicio < fim) {
-                let andar = inicio + passo;
-            
-                if (inicio < fim - passo) {
-                  resultado.innerHTML += `Passo ${andar} \u{261E} `;
-                } else {
-                  resultado.innerHTML += `Passo ${andar}.`;
-                }
-            
-                inicio += passo;
-              }
+  
+    if (inicio !== '' && fim !== '' && passo !== '' && passo !== 0 && passo > 0) {
+      inicio = Number(inicio);
+      fim = Number(fim);
+      passo = Number(passo);
+  
+      if (!isNaN(inicio)) {
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = '';
+  
+        if (fim > inicio) {
+          while (inicio <= fim) {
+            let andar = inicio + passo;
+  
+            if (andar <= fim) {
+              resultado.innerHTML += `Passo ${andar} \u{261E} `;
+            } else {
+              resultado.innerHTML += `Passo ${andar}.`;
+            }
+  
+            inicio += passo;
+          }
         } else {
-            alert('Algo deu errado! Certifique-se de preencher os campos corretamente.');
+          while (inicio >= fim) {
+            let andar = inicio - passo;
+  
+            if (andar >= fim) {
+              resultado.innerHTML += `Passo ${andar} \u{261E} `;
+            } else {
+              resultado.innerHTML += `Passo ${andar}.`;
+            }
+  
+            inicio -= passo;
+          }
         }
+      } else {
+        alert('Algo deu errado! Certifique-se de preencher os campos corretamente.');
+      }
     } else {
-        alert('Algo deu errado! Certifique-se de preencher os campos corretamente.')
+      alert('Algo deu errado! Certifique-se de preencher os campos corretamente.');
     }
-
-}
-
-/* let inicio = 1
-let fim = 10
-let passo = 1
-
-if (inicio != null && !isNaN(inicio)) {
-    while (inicio <= fim) {
-        let andar = inicio + passo;
-        console.log(`Passo ${andar}`);
-        inicio += passo; // Atualiza o valor de inicio a cada iteração
-    }
-} else {
-    console.log('Algo deu errado!');
-} */
+  }
+  
