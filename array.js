8a0acd96badc7abea931d9720fbfdb4d811f30f3
gabrielCosta-dev <lima20gabriel@gmail.com>
2023-07-04 16:05:11 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,19 @@
+let num = [5, 8, 2, 9, 3]
+num.push(7)
+num.sort()
+
+for (let a = 0; a < num.length; a++) {
+    //console.log(num[a])
+}
+
+for (let a in num) {
+    /* if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    } */
+    /* console.log(num[a]) */
+}
+
+console.log(num.indexOf(10))
+//console.log(num.length)
+//console.log(`Meu vetor é ${num}`)
