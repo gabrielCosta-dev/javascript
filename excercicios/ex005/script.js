60f31ab7a076ad092b9d908a43b24f2152bcd2ab
gabrielCosta-dev <lima20gabriel@gmail.com>
2023-07-06 13:09:53 -0300

    continuando exercicio

--- excercicios/ex005/script.js
+++ excercicios/ex005/script.js
@@ -4,19 +4,33 @@ function calcular() {
     let num = document.querySelector('input#numero').value
     let add = document.querySelector('select#adicionados')
     let resposta = document.querySelector('div#resposta')
-    
 
     if (num !== '' && num >= 1 && num <= 100) {
         let numero = Number(num)
-        let resposta = ''
+        //resposta.innerHTML = ''
         if (!(vetor.includes(numero))) {
-            let item = document.createElement('option')
-            add.appendChild(item)
-            item.innerHTML = `Vetor ${numero} foi adicionado.`
+            //Criação option e edição select
+            let option = document.createElement('option')
+            add.appendChild(option)
+            option.innerHTML = `Vetor ${numero} foi adicionado.`
             
+            //Modificações Array
             vetor.push(numero)
-            vetor.sort()
-            console.log(vetor)
+            vetor.sort(function(a , b){
+                return a - b
+            })
+            let menor = vetor[0]
+            let maior = vetor.pop()
+            console.log(maior)
+
+            //Modificações Resposta
+            let paragrafo = document.createElement('p')
+            paragrafo.innerHTML = `O menor valor no vetor é <strong>${menor}!</strong>`
+            resposta.appendChild(paragrafo)
+
+            /* console.log(`Este é ${numero}`)
+            console.log(menor)
+            console.log(vetor) */
         } else {
             alert('Este número já está incluso na array. Digite outro número.')
         }
@@ -25,3 +39,8 @@ function calcular() {
     }
     
 }
+
+/* function limpar() {
+    vetor = []
+    console.log(vetor)
+} */
